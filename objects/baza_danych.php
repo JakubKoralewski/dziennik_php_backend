ad1f87dec9b8224a7c72449d0a29a822e2d5e826
Jakub Koralewski <jakub.koralewski1@gmail.com>
2019-03-04 16:49:09 +0100

    baza_danych: **try** using postgres

--- objects/baza_danych.php
+++ objects/baza_danych.php
@@ -1,20 +1,16 @@
 <?php
 class Database {
- 
-	// specify your own database credentials
-	private $host = "localhost";
-	private $db_name = "projekt_php";
-	private $username = "root";
-	private $password = "";
+
 	public $conn;
  
 	// get the database connection
 	public function getConnection(){
  
 		$this->conn = null;
+		$dbopts = parse_url(getenv('DATABASE_URL'));
  
 		try {
-			$this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+			$this->conn = new PDO("pgsql:host=" . $dbopts["host"] . ";dbname=" . ltrim($dbopts["path"],'/') . ";user=" . $dbopts["user"]. ";password=" . $dbopts["pass"]);
 			$this->conn->exec("set names utf8");
 		} catch(PDOException $exception){
 			echo "Connection error: " . $exception->getMessage();
