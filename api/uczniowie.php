141b21d7cac0235c891017a3b2b8d65628dd849a
Jakub Koralewski <jakub.koralewski1@gmail.com>
2019-03-09 14:33:13 +0100

    allow PUT requests

--- api/uczniowie.php
+++ api/uczniowie.php
@@ -1,6 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Methods: POST, GET, DELETE");
+header("Access-Control-Allow-Methods: POST, GET, DELETE, PUT");
 header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Origin, Access-Control-Allow-Credentials');
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 	exit;
